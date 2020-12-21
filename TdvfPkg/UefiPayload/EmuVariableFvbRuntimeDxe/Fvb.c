8a814b531f30237fa95cf0819ad4e150108f9c17
Wei Liu <weix.c.liu@intel.com>
2020-12-21 16:36:39 +0800

    Add sanity check for CFV data
    Signed-off-by: Wei Liu <weix.c.liu@intel.com>

--- TdvfPkg/UefiPayload/EmuVariableFvbRuntimeDxe/Fvb.c
+++ TdvfPkg/UefiPayload/EmuVariableFvbRuntimeDxe/Fvb.c
@@ -595,6 +595,154 @@ ValidateFvHeader (
   return EFI_SUCCESS;
 }
 
+/**
+  Check padding data all bit should be 1.
+
+  @param[in] Buffer     - A pointer to buffer header
+  @param[in] BufferSize - Buffer size
+
+  @retval  TRUE   - The padding data is valid.
+  @retval  TRUE  - The padding data is invalid.
+
+**/
+BOOLEAN 
+CheckPaddingData (
+  IN UINT8    *Buffer,
+  IN UINT32    BufferSize
+  ) 
+ {
+   UINT32 index;
+
+   for (index = 0; index < BufferSize; index++) {
+     if (Buffer[index] != 0xFF) {
+       return FALSE;
+     }
+   }
+
+   return TRUE;
+ } 
+
+/**
+  Check the integrity of CFV data.
+
+  @param[in] TdvfCfvBase - A pointer to CFV header
+  @param[in] TdvfCfvSize - CFV data size
+
+  @retval  TRUE   - The CFV data is valid.
+  @retval  FALSE  - The CFV data is invalid.
+
+**/
+BOOLEAN 
+ValidateTdvfCfv (
+  IN UINT8    *TdvfCfvBase,
+  IN UINT32    TdvfCfvSize
+  ) 
+{
+  UINT16                         Checksum;
+  UINTN                          VariableBase;
+  UINT32                         VariableOffset = 0;
+  UINT32                         VariableOffsetBeforeAlign;
+  EFI_FIRMWARE_VOLUME_HEADER     *CfvFvHeader;
+  VARIABLE_STORE_HEADER          *CfvVariableStoreHeader;
+  AUTHENTICATED_VARIABLE_HEADER  *VariableHeader;
+  
+  static EFI_GUID FvHdrGUID = EFI_SYSTEM_NV_DATA_FV_GUID;
+  static EFI_GUID VarStoreHdrGUID = EFI_AUTHENTICATED_VARIABLE_GUID;
+
+  if (TdvfCfvBase == NULL) {
+    DEBUG ((DEBUG_ERROR, "TDVF CFV: CFV pointer is NULL\n"));
+    return FALSE;
+  }
+
+  if (TdvfCfvSize > EMU_FVB_SIZE) {
+    DEBUG ((DEBUG_ERROR, "TDVF CFV: CFV size larger than EMU_FVB_SIZE\n"));
+    return FALSE;
+  }
+  
+  //
+  // Verify the header zerovetor, filesystemguid, 
+  // revision, signature, attributes, fvlength, checksum
+  // HeaderLength cannot be an odd number
+  //   
+  CfvFvHeader = (EFI_FIRMWARE_VOLUME_HEADER *) TdvfCfvBase;
+
+  if ((!IsZeroBuffer(CfvFvHeader->ZeroVector, 16)) ||
+      (!CompareGuid(&FvHdrGUID, &CfvFvHeader->FileSystemGuid)) ||
+      (CfvFvHeader->FvLength != TdvfCfvSize) || 
+      (CfvFvHeader->Signature != EFI_FVH_SIGNATURE) ||
+      (CfvFvHeader->Attributes != 0x4feff) ||
+      (CfvFvHeader->HeaderLength != EMU_FV_HEADER_LENGTH) ||
+      (CfvFvHeader->Revision != EFI_FVH_REVISION)
+      ) {
+    DEBUG ((DEBUG_ERROR, "TDVF CFV: Basic FV headers were invalid\n"));
+    return FALSE;
+  }
+
+  //
+  // Verify the header checksum
+  //
+  Checksum = CalculateSum16((VOID*) CfvFvHeader, CfvFvHeader->HeaderLength);
+
+  if (Checksum != 0) {
+    DEBUG ((DEBUG_ERROR, "TDVF CFV: FV checksum was invalid\n"));
+    return FALSE;
+  }
+  
+  //
+  // Verify the header signature, size, format, state
+  //  
+  CfvVariableStoreHeader = (VARIABLE_STORE_HEADER *) (TdvfCfvBase + CfvFvHeader->HeaderLength);
+  if ((!CompareGuid(&VarStoreHdrGUID, &CfvVariableStoreHeader->Signature)) ||
+      (CfvVariableStoreHeader->Size != (CfvFvHeader->FvLength - CfvFvHeader->HeaderLength)) ||
+      (CfvVariableStoreHeader->Format != VARIABLE_STORE_FORMATTED) ||
+      (CfvVariableStoreHeader->State != VARIABLE_STORE_HEALTHY)
+      ) {
+    DEBUG ((DEBUG_ERROR, "TDVF CFV: Variable Store header was invalid\n"));
+    return FALSE;
+  }
+
+  //
+  // Verify the header startId, state
+  // Verify data to the end
+  //  
+  VariableBase = (UINTN)TdvfCfvBase + CfvFvHeader->HeaderLength + sizeof(VARIABLE_STORE_HEADER); 
+  while (VariableOffset  < (CfvVariableStoreHeader->Size - sizeof(VARIABLE_STORE_HEADER))) {
+    VariableHeader = (AUTHENTICATED_VARIABLE_HEADER *) (VariableBase + VariableOffset);
+    if (VariableHeader->StartId != VARIABLE_DATA) {
+      if (!CheckPaddingData((UINT8 *)VariableHeader, CfvVariableStoreHeader->Size - sizeof(VARIABLE_STORE_HEADER) - VariableOffset)) {
+        DEBUG ((DEBUG_ERROR, "TDVF CFV: Variable header was invalid\n"));
+        return FALSE;
+      }
+      VariableOffset = CfvVariableStoreHeader->Size - sizeof(VARIABLE_STORE_HEADER);
+    }
+    else {
+      if (!((VariableHeader->State == VAR_IN_DELETED_TRANSITION) ||
+          (VariableHeader->State == VAR_DELETED) ||
+          (VariableHeader->State == VAR_HEADER_VALID_ONLY) ||
+          (VariableHeader->State == VAR_ADDED))) {
+        DEBUG ((DEBUG_ERROR, "TDVF CFV: Variable header was invalid\n"));
+        return FALSE;
+      }
+
+      VariableOffset += sizeof(AUTHENTICATED_VARIABLE_HEADER) + VariableHeader->NameSize + VariableHeader->DataSize;
+      VariableOffsetBeforeAlign = VariableOffset;
+      // 4 byte align
+      VariableOffset  = (VariableOffset  + 3) & (UINTN)(~3); 
+
+      if (!CheckPaddingData((UINT8 *)(VariableBase + VariableOffsetBeforeAlign), VariableOffset - VariableOffsetBeforeAlign)) {
+        DEBUG ((DEBUG_ERROR, "TDVF CFV: Variable header was invalid\n"));
+        return FALSE;
+      }
+    }
+  }
+
+  if ((VariableBase + VariableOffset) != ((UINTN)TdvfCfvBase + TdvfCfvSize)) {
+    DEBUG ((DEBUG_ERROR, "TDVF CFV: Top address of CFV does not match\n"));
+    return FALSE;
+  }
+
+  return TRUE;
+}
 
 /**
   Initializes the FV Header and Variable Store Header
@@ -778,10 +926,11 @@ FvbInitialize (
     DEBUG((DEBUG_INFO, "Copy the content from Configuration FV\n"));
     CfvBase = (UINT8*)(UINTN)PcdGet64(PcdTdvfCfvBase);
     CfvSize = (UINT32)PcdGet64(PcdTdvfCfvSize);
-    ASSERT(CfvBase != 0);
-    ASSERT(CfvSize <= EMU_FVB_SIZE);
-    CopyMem(Ptr, CfvBase, CfvSize);
-    Initialize = FALSE;
+    
+    if(ValidateTdvfCfv(CfvBase, CfvSize)) {
+      CopyMem(Ptr, CfvBase, CfvSize);
+      Initialize = FALSE;
+    }   
   }
 
   //
