8599b6a9d1d3de1310a4ab89ff587abe3f696dcd
Min Xu <min.m.xu@intel.com>
2021-04-22 15:05:44 +0800

    OvmfPkg: Support 2M page accept size
    TDCALL(ACCEPT_PAGE) supports the accept page size of 4k and 2M. To
    simplify the implementation, the Memory to be accpeted is splitted
    into 3 parts:
      -----------------  <-- StartAddress1 (not 2M aligned)
      |  part 1       |      Length1 < 2M
      |---------------|  <-- StartAddress2 (2M aligned)
      |               |      Length2 = Integer multiples of 2M
      |  part 2       |
      |               |
      |---------------|  <-- StartAddress3
      |  part 3       |      Length3 < 2M
      |---------------|
    
    part 1) will be accepted in 4k and by BSP.
    part 2) will be accepted in 2M and by BSP/AP.
    Part 3) will be accepted in 4k and by BSP.

--- OvmfPkg/Library/TdxStartupLib/Mp.c
+++ OvmfPkg/Library/TdxStartupLib/Mp.c
@@ -16,6 +16,8 @@
 #include <Library/SynchronizationLib.h>
 #include "TdxStartupInternal.h"
 
+#define ALIGNED_2MB_MASK 0x1fffff
+
 /**
   This function will be called by BSP to wakeup APs the are spinning on mailbox
   in protected mode
@@ -93,6 +95,68 @@ MpSerializeEnd (
   InterlockedDecrement ((UINT32 *) &MailBox->NumCpusExiting);
 }
 
+EFI_STATUS
+EFIAPI
+BspAcceptMemoryResourceRange (
+  IN EFI_PHYSICAL_ADDRESS   StartAddress,
+  IN UINT64                 Length,
+  IN UINT64                 AcceptChunkSize,
+  IN UINT64                 AcceptPageSize
+  )
+{
+  EFI_STATUS                  Status;
+  UINT64                      Pages;
+  UINT64                      Stride;
+  EFI_PHYSICAL_ADDRESS        PhysicalAddress;
+  volatile MP_WAKEUP_MAILBOX  *MailBox;
+
+  Status = EFI_SUCCESS;
+  PhysicalAddress = StartAddress;
+  Stride = GetNumCpus() * AcceptChunkSize;
+  MailBox = (volatile MP_WAKEUP_MAILBOX  *)GetMailBox();
+
+  while (!EFI_ERROR(Status) && PhysicalAddress < StartAddress + Length) {
+    //
+    // Decrease size of near end of resource if needed.
+    //
+    Pages = RShiftU64(MIN(AcceptChunkSize, Length), EFI_PAGE_SHIFT);
+
+    MailBox->Tallies[0] += (UINT32)Pages;
+
+    Status = TdAcceptPages (PhysicalAddress, Pages, AcceptPageSize);
+    //
+    // Bump address to next chunk this cpu is responisble for
+    //
+    PhysicalAddress += Stride;
+  }
+
+  return Status;
+}
+
+/**
+  This function will be called to accept pages. BSP and APs are invokded
+  to do the task together.
+
+  TDCALL(ACCEPT_PAGE) supports the accept page size of 4k and 2M. To
+  simplify the implementation, the Memory to be accpeted is splitted
+  into 3 parts:
+  -----------------  <-- StartAddress1 (not 2M aligned)
+  |  part 1       |      Length1 < 2M
+  |---------------|  <-- StartAddress2 (2M aligned)
+  |               |      Length2 = Integer multiples of 2M
+  |  part 2       |
+  |               |
+  |---------------|  <-- StartAddress3
+  |  part 3       |      Length3 < 2M
+  |---------------|
+
+  part 1) will be accepted in 4k and by BSP.
+  Part 2) will be accepted in 2M and by BSP/AP.
+  Part 3) will be accepted in 4k and by BSP.
+
+  @param[in] PhysicalAddress   Start physical adress
+  @param[in] PhysicalEnd       End physical address
+**/
 VOID
 EFIAPI
 MpAcceptMemoryResourceRange (
@@ -100,49 +164,126 @@ MpAcceptMemoryResourceRange (
   IN EFI_PHYSICAL_ADDRESS        PhysicalEnd
   )
 {
-  UINT64                      Pages;
-  UINT64                      Stride;
+  EFI_STATUS                  Status;
   UINT64                      AcceptChunkSize;
   UINT64                      AcceptPageSize;
+  UINT64                      StartAddress1;
+  UINT64                      StartAddress2;
+  UINT64                      StartAddress3;
+  UINT64                      TotalLength;
+  UINT64                      Length1;
+  UINT64                      Length2;
+  UINT64                      Length3;
   volatile MP_WAKEUP_MAILBOX  *MailBox;
 
-  MailBox = (volatile MP_WAKEUP_MAILBOX  *)GetMailBox();
-  AcceptChunkSize = FixedPcdGet64(PcdTdxAcceptChunkSize);
-  AcceptPageSize = FixedPcdGet64(PcdTdxAcceptPageSize);
+  MailBox = (volatile MP_WAKEUP_MAILBOX  *)GetMailBox ();
+  AcceptChunkSize = FixedPcdGet64 (PcdTdxAcceptChunkSize);
+  AcceptPageSize = FixedPcdGet64 (PcdTdxAcceptPageSize);
+  TotalLength = PhysicalEnd - PhysicalAddress;
+  StartAddress1 = 0;
+  StartAddress2 = 0;
+  StartAddress3 = 0;
+  Length1 = 0;
+  Length2 = 0;
+  Length3 = 0;
+
+  if (AcceptPageSize == SIZE_4KB || TotalLength <= SIZE_2MB) {
+    //
+    // if total length is less than 2M, then we accept pages in 4k
+    //
+    StartAddress1 = 0;
+    Length1 = 0;
+    StartAddress2 = PhysicalAddress;
+    Length2 = PhysicalEnd - PhysicalAddress;
+    StartAddress3 = 0;
+    Length3 = 0;
+  } else if (AcceptPageSize == SIZE_2MB) {
+    //
+    // Total length is bigger than 2M and Page Accept size 2M is supported.
+    //
+    if ((PhysicalAddress & ALIGNED_2MB_MASK) == 0) {
+      //
+      // Start address is 2M aligned
+      //
+      StartAddress1 = 0;
+      Length1 = 0;
+      StartAddress2 = PhysicalAddress;
+      Length2 = TotalLength & ALIGNED_2MB_MASK;
+
+      if (TotalLength > Length2) {
+        //
+        // There is remaining part 3)
+        //
+        StartAddress3 = StartAddress2 + Length2;
+        Length3 = TotalLength - Length2;
+        ASSERT (Length3 < SIZE_2MB);
+      }
+    } else {
+      //
+      // Start address is not 2M aligned and total length is bigger than 2M.
+      //
+      StartAddress1 = PhysicalAddress;
+      ASSERT (TotalLength > SIZE_2MB);
+      Length1 = SIZE_2MB - (PhysicalAddress & ALIGNED_2MB_MASK);
+      if (TotalLength - Length1 < SIZE_2MB) {
+        //
+        // The Part 2) length is less than 2MB, so let's accept all the
+        // memory in 4K
+        //
+        Length1 = TotalLength;
+
+      } else {
+        StartAddress2 = PhysicalAddress + Length1;
+        Length2 = (TotalLength - Length1) & ALIGNED_2MB_MASK;
+        StartAddress3 = StartAddress2 + Length2;
+        Length3 = TotalLength - Length1 - Length2;
+        ASSERT (Length3 < SIZE_2MB);
+      }
+    }
+  }
+
+  DEBUG ((DEBUG_INFO, "TdAccept: 0x%llx - 0x%llx\n", PhysicalAddress, TotalLength));
+  DEBUG ((DEBUG_INFO, "   Part1: 0x%llx - 0x%llx\n", StartAddress1, Length1));
+  DEBUG ((DEBUG_INFO, "   Part2: 0x%llx - 0x%llx\n", StartAddress2, Length2));
+  DEBUG ((DEBUG_INFO, "   Part3: 0x%llx - 0x%llx\n", StartAddress3, Length3));
 
   MpSerializeStart();
 
-  MpSendWakeupCommand(MpProtectedModeWakeupCommandAcceptPages,
-    0,
-    PhysicalAddress,
-    PhysicalEnd,
-    AcceptChunkSize,
-    AcceptPageSize);
+  if (Length2 > 0) {
+    MpSendWakeupCommand (
+      MpProtectedModeWakeupCommandAcceptPages,
+      0,
+      StartAddress2,
+      StartAddress2 + Length2,
+      AcceptChunkSize,
+      AcceptPageSize);
 
-  //
-  // All cpus share the burden of accepting the pages
-  // A cpu will accept AcceptSize size amount of memory
-  // and then skip pass range the other cpus do
-  // Stride is the amount of skip
-  //
-  Stride = GetNumCpus() * AcceptChunkSize;
-  //
-  // Keep accepting until end of resource
-  //
-  while (PhysicalAddress < PhysicalEnd) {
-    //
-    // Decrease size of near end of resource if needed.
-    //
-    Pages = RShiftU64(MIN(AcceptChunkSize, PhysicalEnd - PhysicalAddress), EFI_PAGE_SHIFT);
-
-    MailBox->Tallies[0] += (UINT32)Pages;
-
-    TdAcceptPages ( PhysicalAddress, Pages, AcceptPageSize);
-    //
-    // Bump address to next chunk this cpu is responisble for
-    //
-    PhysicalAddress += Stride;
+      Status = BspAcceptMemoryResourceRange (
+                  StartAddress2,
+                  Length2,
+                  AcceptChunkSize,
+                  AcceptPageSize);
+      ASSERT (!EFI_ERROR (Status));
   }
+
+  if (Length1 > 0) {
+    Status = BspAcceptMemoryResourceRange (
+                StartAddress1,
+                Length1,
+                AcceptChunkSize,
+                SIZE_4KB);
+    ASSERT (!EFI_ERROR (Status));
+  }
+
+  if (Length3 > 0) {
+    Status = BspAcceptMemoryResourceRange (
+                StartAddress3,
+                Length3,
+                AcceptChunkSize,
+                SIZE_4KB);
+    ASSERT (!EFI_ERROR (Status));
+  }
+
   MpSerializeEnd();
 
   DEBUG((DEBUG_INFO, "Tallies %x %x %x %x %x %x %x %x\n",
