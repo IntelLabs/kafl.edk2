b07a8f73672f19e46443a3086738baaabb726e8b
Min M Xu <min.m.xu@intel.com>
2021-04-23 15:23:46 +0800

    TDVF: Code base of TDVF One Binary
    Feature of TDVF One Binary requires to run the TDVF on both Td and Non-Td
    guest. Its code base is commit of 6e55868631 in edk2-master branch.

---
+++ ArmPkg/Library/ArmLib/AArch64/AArch64Lib.c
@@ -0,0 +1,89 @@
+/** @file
+
+  Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
+  Portions copyright (c) 2011 - 2014, ARM Ltd. All rights reserved.<BR>
+  Copyright (c) 2020, NUVIA Inc. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Base.h>
+
+#include <Library/ArmLib.h>
+#include <Library/DebugLib.h>
+
+#include <Chipset/AArch64.h>
+
+#include "AArch64Lib.h"
+#include "ArmLibPrivate.h"
+
+VOID
+AArch64DataCacheOperation (
+  IN  AARCH64_CACHE_OPERATION  DataCacheOperation
+  )
+{
+  UINTN     SavedInterruptState;
+
+  SavedInterruptState = ArmGetInterruptState ();
+  ArmDisableInterrupts();
+
+  AArch64AllDataCachesOperation (DataCacheOperation);
+
+  ArmDataSynchronizationBarrier ();
+
+  if (SavedInterruptState) {
+    ArmEnableInterrupts ();
+  }
+}
+
+VOID
+EFIAPI
+ArmInvalidateDataCache (
+  VOID
+  )
+{
+  ASSERT (!ArmMmuEnabled ());
+
+  ArmDataSynchronizationBarrier ();
+  AArch64DataCacheOperation (ArmInvalidateDataCacheEntryBySetWay);
+}
+
+VOID
+EFIAPI
+ArmCleanInvalidateDataCache (
+  VOID
+  )
+{
+  ASSERT (!ArmMmuEnabled ());
+
+  ArmDataSynchronizationBarrier ();
+  AArch64DataCacheOperation (ArmCleanInvalidateDataCacheEntryBySetWay);
+}
+
+VOID
+EFIAPI
+ArmCleanDataCache (
+  VOID
+  )
+{
+  ASSERT (!ArmMmuEnabled ());
+
+  ArmDataSynchronizationBarrier ();
+  AArch64DataCacheOperation (ArmCleanDataCacheEntryBySetWay);
+}
+
+/**
+  Check whether the CPU supports the GIC system register interface (any version)
+
+  @return   Whether GIC System Register Interface is supported
+
+**/
+BOOLEAN
+EFIAPI
+ArmHasGicSystemRegisters (
+  VOID
+  )
+{
+  return ((ArmReadIdAA64Pfr0 () & AARCH64_PFR0_GIC) != 0);
+}
