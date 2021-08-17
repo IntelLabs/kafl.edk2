ea2abe73148852e04db1f8352eae69f7f5aec906
Jiaqi Gao <jiaqi.gao@intel.com>
2021-08-17 16:01:57 +0800

    OvmfPkg: Correct the PCR index used by BootVariable measurement
    According to section 8.1 of TDVF design guid, BootVariable should be
    measured into RTMR[1] (PCR[5]).
    Avoid RTMR[0] inconsistency caused by BootOption updates during the
    first boot.
    
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- OvmfPkg/Tcg/Tcg2Dxe/TdTcg2Dxe.c
+++ OvmfPkg/Tcg/Tcg2Dxe/TdTcg2Dxe.c
@@ -1955,8 +1955,12 @@ ReadAndMeasureBootVariable (
   OUT     VOID                      **VarData
   )
 {
+  //
+  // Boot variables are measured into (PCR[5]) RTMR[1],
+  // details in section 8.1 of TDVF design guide.
+  //
   return ReadAndMeasureVariable (
-           1,
+           5,
            EV_EFI_VARIABLE_BOOT,
            VarName,
            VendorGuid,
